ef666621f94a43395fa9fa4596f2c57c50354938
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-08 22:45:42 +0300

    Local ip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-PUBLIC_IP_ADDRESS=$( wget -O - http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]' )
+PUBLIC_IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 echo "BROADCAST ADDRESS: $PUBLIC_IP_ADDRESS"
 
 exec nsqd "$@" "--data-path=/data" "--broadcast-address=$PUBLIC_IP_ADDRESS"
