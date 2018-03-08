1f3e86616fcb54c433d5b95a7013555a89ebd633
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-08 23:10:30 +0300

    Install curl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM nsqio/nsq
 
+# Install prerequisites
+RUN apt-get update && apt-get install -y \
+	curl
+
 ADD ./docker-entrypoint.sh /
 RUN chmod +x ./docker-entrypoint.sh
 ENTRYPOINT ["/bin/sh", "./docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-PUBLIC_IP_ADDRESS=$(wget -qO- http://169.254.169.254/latest/meta-data/local-ipv4)
+PUBLIC_IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 echo "BROADCAST ADDRESS: $PUBLIC_IP_ADDRESS"
 
 exec nsqd "$@" "--data-path=/data" "--broadcast-address=$PUBLIC_IP_ADDRESS"
