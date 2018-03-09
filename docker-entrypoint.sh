1d5d5d25589e60559efecee0b8cf34b5ed952ade
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-09 12:52:43 +0300

    Using public ip again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-IP_ADDRESS=$(wget -qO- http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(wget -O - http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]')
 echo "BROADCAST ADDRESS: $IP_ADDRESS"
 
 exec nsqd "$@" "--broadcast-address=$IP_ADDRESS"
