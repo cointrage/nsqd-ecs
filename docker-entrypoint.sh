c28dbbb8bcfc6a8f5bc9f0da7d860c3981f5fbfc
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-09 14:20:43 +0300

    Printing ecs metadata file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cat $ECS_CONTAINER_METADATA_FILE
+
 IP_ADDRESS=$(wget -O - http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]')
 echo "BROADCAST ADDRESS: $IP_ADDRESS"
 
