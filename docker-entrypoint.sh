5e4a95508c2eea2310d2d671513816243346ad0e
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-10 10:17:08 +0300

    Public ip via ec2 metadata

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "BROADCAST ADDRESS: $IP_ADDRESS"
 
 exec nsqd "$@" "--broadcast-address=$IP_ADDRESS"
